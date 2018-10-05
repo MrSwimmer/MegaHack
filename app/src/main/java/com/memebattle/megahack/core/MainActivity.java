f7a5fed8f7df6948ef711d0466cfc9a61595ed85
LukianBat <lukianzhukov2000@gmail.com>
2018-10-05 22:54:15 +0700

    first

---
+++ app/src/main/java/com/memebattle/megahack/core/MainActivity.java
@@ -0,0 +1,39 @@
+package com.memebattle.megahack.core;
+
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.FrameLayout;
+
+import com.memebattle.megahack.R;
+
+
+public class MainActivity extends AppCompatActivity {
+    BottomNavigationView bottomNavigationView;
+    FrameLayout container;
+    FragmentManager fragmentManager;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        container = findViewById(R.id.container);
+
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        bottomNavigationView.setSelectedItemId(R.id.request);
+        bottomNavigationView.setOnNavigationItemSelectedListener(menuItem -> {
+            switch (menuItem.getItemId()) {
+                case R.id.auth: {
+                    break;
+                }
+                case R.id.request: {
+                    break;
+                }
+                case R.id.settings: {
+                    break;
+                }
+            }
+            return true;
+        });
+    }
+}
