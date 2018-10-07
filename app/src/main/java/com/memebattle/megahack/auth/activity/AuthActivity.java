f30c43654498b66cd8e5a8fc2c244100f155d8a4
LukianBat <lukianzhukov2000@gmail.com>
2018-10-07 11:53:47 +0700

    5

--- app/src/main/java/com/memebattle/megahack/auth/activity/AuthActivity.java
+++ app/src/main/java/com/memebattle/megahack/auth/activity/AuthActivity.java
@@ -1,7 +1,6 @@
 package com.memebattle.megahack.auth.activity;
 
 
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentTransaction;
@@ -10,7 +9,6 @@ import android.support.v7.app.AppCompatActivity;
 import com.memebattle.megahack.App;
 import com.memebattle.megahack.R;
 import com.memebattle.megahack.auth.fragment.sign_in.SignInFragment;
-import com.memebattle.megahack.auth.fragment.sign_up.SignUpFragment;
 import com.memebattle.megahack.main.activity.MainActivity;
 
 public class AuthActivity extends AppCompatActivity {
@@ -25,7 +23,7 @@ public class AuthActivity extends AppCompatActivity {
 
         String launch = App.settingsService.getUserId();
 
-        if (launch.equals("error")) {
+        if (!launch.equals("error")) {
             startActivity(new Intent(AuthActivity.this, MainActivity.class));
             this.finish();
         }else{
